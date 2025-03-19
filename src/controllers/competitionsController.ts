c6fcfb5c29420711bab8aad72eb103fc56041158
pranav-027 <pd2772001@gmail.com>
2025-03-19 13:36:57 +0530

    query params for rankings controller

--- src/controllers/competitionsController.ts
+++ src/controllers/competitionsController.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
 
-
+const COMPS_S3_URL = 'https://sci-temporary-bucket.s3.us-west-2.amazonaws.com/competition.json';
 const competitions = async (req: Request, res: Response): Promise<void> => {
   try {
-    const compsS3DATAURL = 'https://sci-temporary-bucket.s3.us-west-2.amazonaws.com/competition.json';
-    const response = await axios.get(compsS3DATAURL);
+    const response = await axios.get(COMPS_S3_URL);
     res.json(response.data);
   } catch (error: any) {
     res.status(500).json({ message: error.message });
--- src/controllers/rankingsController.ts
+++ src/controllers/rankingsController.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
 
+const RANKINGS_S3_URL = 'https://sci-temporary-bucket.s3.us-west-2.amazonaws.com/rankings.json';
+
 const rankings = async (req: Request, res: Response): Promise<void> => {
   try {
-    const rankingsS3DataUrl = 'https://sci-temporary-bucket.s3.us-west-2.amazonaws.com/rankings.json';
-    const response = await axios.get(rankingsS3DataUrl);
-    res.json(response.data);
+
+    const eventId = (req.query.eventId as string) || "333";
+    const stateId = (req.query.stateId as string) || "MH";
+    const type = (req.query.type as string) || "single";
+
+    const response = await axios.get(RANKINGS_S3_URL);
+    const rankingsData = response.data;
+
+    const key = `${eventId}-${stateId}-${type}`;
+
+    res.json(rankingsData[key] || []);
+
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
