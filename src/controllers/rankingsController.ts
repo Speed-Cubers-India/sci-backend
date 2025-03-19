9a0ce6f42c06a74be25ca9b4a0918af79126810a
pranav-027 <pd2772001@gmail.com>
2025-03-19 13:59:25 +0530

    400 error for missing query param

--- src/controllers/rankingsController.ts
+++ src/controllers/rankingsController.ts
@@ -5,18 +5,18 @@ const RANKINGS_S3_URL = 'https://sci-temporary-bucket.s3.us-west-2.amazonaws.com
 
 const rankings = async (req: Request, res: Response): Promise<void> => {
   try {
+    const { eventId, stateId, type } = req.query;
 
-    const eventId = (req.query.eventId as string) || "333";
-    const stateId = (req.query.stateId as string) || "MH";
-    const type = (req.query.type as string) || "single";
+    if (!eventId || !stateId || !type) {
+      res.status(400).json({ message: "Missing required query parameters: eventId, stateId, type" });
+    } else {
+      const response = await axios.get(RANKINGS_S3_URL);
+      const rankingsData = response.data;
 
-    const response = await axios.get(RANKINGS_S3_URL);
-    const rankingsData = response.data;
+      const key = `${eventId}-${stateId}-${type}`;
 
-    const key = `${eventId}-${stateId}-${type}`;
-
-    res.json(rankingsData[key] || []);
-
+      res.json(rankingsData[key] || []);
+    }
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
