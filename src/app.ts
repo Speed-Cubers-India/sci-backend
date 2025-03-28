4b3f1b78eadc6896b3d7fa08d111d6cf7103382f
Allen John <allenjohnmonapallil@gmail.com>
2025-03-28 15:19:39 +0530

    update: rebase

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express, { Application } from "express";
-import router from "./routes";
-import auth from "./routes/auth";
+import express, { Application } from 'express';
+import router from './routes';
+import auth from './routes/auth';
+import corsOptions from './config/corsOptions';
 
 // Initialize the express app.
 const app: Application = express();
+
+// Enable CORS.
+app.use(corsOptions);
 
 // Middleware setup.
 app.use(express.json());
