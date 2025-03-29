b780bd0b878898e080666fefffa846f3c1502eef
Allen John <allenjohnmonapallil@gmail.com>
2025-03-29 09:57:36 +0530

    add: all current routes to single file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application } from 'express';
-import router from './routes';
-import auth from './routes/auth';
 import corsOptions from './config/corsOptions';
+import router from './routes';
 
 // Initialize the express app.
 const app: Application = express();
@@ -12,7 +11,6 @@ app.use(corsOptions)
 // Middleware setup.
 app.use(express.json());
 app.use("/", router);
-app.use("/api/auth", auth);
 
 // Export the app to be used elsewhere.
 export default app;
