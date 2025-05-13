53c82deb7b3f82002a981bbe8340d27a9c14ccbf
Allen John <allenjohnmonapallil@gmail.com>
2025-05-13 12:33:52 -0600

    api for team info

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import competitions from './controllers/competitionsController';
 import rankings from "./controllers/rankingsController";
 import { callback, login } from './controllers/authController';
+import { teams } from './controllers/getTeams';
 
 const router: Router = Router();
 
 router.get('/competitions', competitions);
 router.get('/rankings', rankings);
+router.get('/teams', teams)
 router.get('/api/auth/login', login)
 router.get('/api/auth/callback', callback)
 
