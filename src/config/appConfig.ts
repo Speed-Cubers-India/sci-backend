96a96e02349dffa1604d8747b2410fac90d2ee13
Allen John <allenjohnmonapallil@gmail.com>
2025-07-05 23:29:40 -0600

    minor typos

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 const appConfig: AppConfig = {
   port: parseInt(process.env.PORT || '4000', 10),
-  frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3000'
+  frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3000',
 };
 
 export default appConfig;
